c2992cdc8823d0a4d10f9aaee0d38d7e74e9da3f
Eduardo González <eduardo.gonzalez5599@gmail.com>
2020-10-20 23:49:30 -0300

    Primer Commit

---
+++ docs/precache-manifest.ed003974f8eb295fd1ceace1e4fc305f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06632f228f993361f08b4f43b157c787",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99329deb3555b014eb1f",
+    "url": "/static/css/main.77b076ac.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99329deb3555b014eb1f",
+    "url": "/static/js/main.a5896d34.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
